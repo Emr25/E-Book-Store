b537b11a1db01ab11ebdae809327c09c164b0607
Emre <emrecan2525@gmail.com>
2025-07-07 17:05:48 +0300

    Update Settings

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
+
   ],
+  base: process.env.VITE_BASE_PATH || "/E-Book-Store"
 })
