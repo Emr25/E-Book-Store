ef7ef4fd9a456bd5b58dc5f906ab4f0349b8cae1
Emre <emrecan2525@gmail.com>
2025-07-07 17:17:43 +0300

    App Jsx AuthSlice Name Error Solve

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -6,7 +6,7 @@ import Login from './components/Login ';
 import Header from './components/Header ';
 import Register from './components/Register ';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUser } from './redux/authSlice';
+import { getUser } from './redux/AuthSlice';
 import ProductList from './components/ProductList ';
 import "./App.css"
 import ProductDetails from './components/ProductDetails ';
